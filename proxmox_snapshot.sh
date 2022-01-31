265701bdc3da8fe9565cef04824622c22ed19283
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-31 18:02:31 +0000

    Improvements to bash helpers

--- proxmox_snapshot.sh
+++ proxmox_snapshot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /opt/proxmox-zfs-snapshots
 source bin/activate
-python3 proxmox_snapshot.py @
+python3 proxmox_snapshot.py $@
--- proxmox_snapshot_updater.sh
+++ proxmox_snapshot_updater.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+cd /opt/proxmox-zfs-snapshots
+git pull
+cat proxmox_snapshot.sh > /bin/proxmox_snapshot
+chmod +x /bin/proxmox_snapshot
